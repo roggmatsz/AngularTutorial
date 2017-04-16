cd579b973b6ed8c0336b943789ebfab52cd376a5
Rogger Matamoros <roggmatz@gmail.com>
2017-04-15 23:05:30 -0400

    End of part 2.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes';
+  heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
+export class Hero {
+  id: number;
+  name: string;
+}
+const HEROES: Hero[] = [
+  { id: 11, name: 'Chappie' },
+  { id: 12, name: 'Rembrandt' },
+  { id: 13, name: 'Princeton' },
+  { id: 14, name: 'Harvard' },
+  { id: 15, name: 'Stanford' },
+  { id: 16, name: 'Tommy' }
+]
