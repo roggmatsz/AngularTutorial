e7220670ef6d185e4ee2aeab4a7f485df535394a
Rogger Matamoros <roggmatz@gmail.com>
2017-04-18 20:07:08 -0400

    Routing Section, part 1.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,13 @@
 import { Component } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
-import { OnInit } from '@angular/core';
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [HeroService]
+    selector: 'app-root',
+    template: `
+        <h1>
+            {{title}}
+        </h1>
+        <my-heroes></my-heroes>
+    `
 })
-export class AppComponent implements OnInit {
-  title = 'Tour of Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-  getHeroes(): void {
-    this.heroService.getHeroes()
-      .then(heroes => this.heroes = heroes);
-  }
-  constructor(private heroService: HeroService) {}
+export class AppComponent {
+    title = "Tour of Heroes";
 }
